6ac6a83998029d349ea89ffc8152ea6588640cd0
tiago murer <tiagostaurenghi@hotmail.com>
2018-09-11 08:57:02 -0300

    exercicio aula 5

--- src/main/java/br/edu/ifsp/spo/lg2a2/sge/repositories/CursosRepository.java
+++ src/main/java/br/edu/ifsp/spo/lg2a2/sge/repositories/CursosRepository.java
@@ -2,14 +2,28 @@ package br.edu.ifsp.spo.lg2a2.sge.repositories;
 
 import java.util.Collection;
 
+
 import br.edu.ifsp.spo.lg2a2.sge.entidades.Aluno;
 import br.edu.ifsp.spo.lg2a2.sge.entidades.Curso;
 import br.edu.ifsp.spo.lg2a2.sge.entidades.Turma;
 
 public class CursosRepository {
 	
+	private static Collection<Aluno> alunos = new ArrayList<Aluno>();
+	private static Collection<Turma> turmas = new ArrayList<Turma>();
+	private static Collection<Curso> cursos = new ArrayList<Curso>();
+	
+	
 	public Curso buscarPorCodigo(String codigo) {
-		return null;
+		Curso resultado = null;
+		
+		for(Curso curso : CursosRepository.cursos) {
+			if(curso.getCodigo().equals(codigo)) {
+				resultado = curso;
+				break;
+			}
+			
+		return curso;
 	}
 	
 	public Collection<Turma> buscarTurmas(String codigoCurso){
@@ -21,9 +35,16 @@ public class CursosRepository {
 	}
 	
 	public boolean existeAlunoNoCurso(String cpf) {
-		return false;
+		Aluno resultado = 0;
+		for(Aluno aluno: AlunosRepository.alunos) {
+			if(aluno.getCpf().equals(cpf)) {
+				resultado++;
+			}
+		}
+		return resultado > 0;
 	}
 	
 	public void adicionarAluno(Aluno aluno) {
+		AlunosRepository.alunos.add(aluno);
 	}
 }
